87649b80cdc1131ca5539ae10cf6578381af1318
ricardoqsx <ricardoqs@me.com>
2025-01-05 22:53:56 -0500

    areglando alguito en el sqlite

--- app/db.py
+++ app/db.py
@@ -9,14 +9,16 @@ usrdb = 'users.db'
 def conn_usr():
     return sqlite3.connect(usrdb)
 
-with conn_usr() as users:
-    cur = users.cursor()
-    cur.execute('''
-        create table if not exists users(
-        id integer primary key autoincrement,
-        username text not null,
-        password text not null,
-        fullname text not null)''')
+def create_users():
+    with conn_usr() as users:
+        cur = users.cursor()
+        cur.execute('''
+            create table if not exists users(
+            id integer primary key autoincrement,
+            username text not null,
+            password text not null,
+            fullname text not null)''')
+
 
 # CRUD operations
 
